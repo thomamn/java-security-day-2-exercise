d6e349504ff545a4546f2dc34abee586a585f3d4
TMajlu <thomasmajnie@gmail.com>
2024-09-18 15:43:02 +0200

    Believe I completed the core.

--- src/main/java/com/booleanuk/api/Library/security/WebSecurityConfig.java
+++ src/main/java/com/booleanuk/api/Library/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import com.booleanuk.api.Library.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -54,7 +55,11 @@ public class WebSecurityConfig {
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/**").permitAll()
                         .requestMatchers("/books", "/books/**").authenticated()
+                        .requestMatchers("/games", "/games/**").authenticated()
+                        .requestMatchers("/dvds", "/dvds/**").authenticated()
+                        .requestMatchers("/cds", "/cds/**").authenticated()
                 );
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
